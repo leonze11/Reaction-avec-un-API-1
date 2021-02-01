92a3c226fc55a92b82dbe0ab4bec0169984bd5f4
leonze11 <nguelohokevin@gmail.com1997fa11git config --global user.email nguelohokevin@gmail.com>
2021-02-01 16:20:05 +0100

    version 1

---
+++ script.js
@@ -0,0 +1,56 @@
+const searchInput = document.getElementById("search");
+const results = document.getElementById("results");
+
+let countries;
+let searchTerm ='';
+//API REQUEST
+
+const fetchCountries = async() =>{
+    countries =await fetch(
+        'https://restcountries.eu/rest/v2/all?fields=name;population;flag'
+    ).then(res => res.json());
+};
+
+const showCountries = async() => {
+    await fetchCountries();
+
+    results.innerHTML = (
+
+        countries
+        .filter(country => country.name.toLowerCase().includes(searchTerm.toLowerCase()))
+        // .filter(country => country.name.includes(searchTerm))
+        .map(country => (
+
+
+            `
+            <li class = "country-item">
+                <img class = "country-flag" src= "${country.flag}"/>
+                <h3 class = "country-name">${country.name}</h3>
+                <div class = "country-info">
+                    <h2 class = "country-population">${numberWithSpace(country.population)}</h2>
+                    <h5 class = "country-population-text">Habitants</h5>
+                </div>
+            </li>
+            `
+
+        )).join('')
+            
+        
+
+    );
+
+
+};
+
+showCountries();
+
+//INPUT SETUP
+
+searchInput.addEventListener('input', (e) => {searchTerm = e.target.value;
+    showCountries();	
+  });
+    
+
+function numberWithSpace(x){
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
